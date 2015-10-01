507a2d443725aa40c6058202b2e6ebb98d649a8f
Scott Giminiani <scottg489@gmail.com>
2015-09-30 22:29:01 -0400

    Pull parsing of header out into a separate function. Don't iterate over every line but just do a single pass through since every state is accounted for anyways. This means that by the end of the first iteration it would be at the end of the header and subsequent iterations would be a waste.

--- src/main/groovy/udp/IndividualDiffParser.groovy
+++ src/main/groovy/udp/IndividualDiffParser.groovy
@@ -17,18 +17,11 @@ class IndividualDiffParser {
         unifiedDiff.setFileStatus(UnifiedDiff.FileStatus.Modified)
     }
 
-    // TODO: Inconsistency between added/deleted text and binary files:
-    // TODO:        Text: To/From file respectively is '/dev/null' along with file being added/deleted
-    // TODO:        Binary: To/From file respectively are both the same
-    // TODO: This should be refactored using 'states' if only for readability
-    // TODO: Should 'binary' 'header' lines be considered part of the header? Or are they
-    // TODO:    really part of the body? Seems like they are more part of the body
-    // TODO:    but since we want to know if the file is binary we parse them as though they are
-    // TODO:    part of the header.
-    UnifiedDiff parse() {
-        unifiedDiff.setDiffHeader(extractDiffHeader(unifiedDiff.getRawDiff()))
+    UnifiedDiff parseHeader() {
         Iterator<String> lineIter = unifiedDiff.getDiffHeader().readLines().iterator()
-        while (lineIter.hasNext()) {
+        // TODO: This likely doesn't need to loop since we step through each line until we
+        // TODO:    get to the end of the header
+        if (lineIter.hasNext()) {
             String diffGitLine = lineIter.next()
             if (!isDiffGitLine(diffGitLine)) {
                 // First line isn't git diff line. Malformed.
@@ -87,7 +80,7 @@ class IndividualDiffParser {
             }
             if (!lineIter.hasNext()) {
                 // End of header
-                break
+                return
             }
             String indexLine = lineIter.next()
             if (isIndexLine(indexLine)) {
@@ -101,7 +94,7 @@ class IndividualDiffParser {
             }
             if (!lineIter.hasNext()) {
                 // No to/from file lines so break
-                break
+                return
             }
 
             String postHeaderLine = lineIter.next()
@@ -118,7 +111,23 @@ class IndividualDiffParser {
                 unifiedDiff.setFromFile(extractFromFileFromBinaryLine(postHeaderLine))
                 unifiedDiff.setToFile(extractToFileFromBinaryLine(postHeaderLine))
             }
+        } else {
+            // empty diff, malformed
         }
+
+        return unifiedDiff
+    }
+    // TODO: Inconsistency between added/deleted text and binary files:
+    // TODO:        Text: To/From file respectively is '/dev/null' along with file being added/deleted
+    // TODO:        Binary: To/From file respectively are both the same
+    // TODO: This should be refactored using 'states' if only for readability
+    // TODO: Should 'binary' 'header' lines be considered part of the header? Or are they
+    // TODO:    really part of the body? Seems like they are more part of the body
+    // TODO:    but since we want to know if the file is binary we parse them as though they are
+    // TODO:    part of the header.
+    UnifiedDiff parse() {
+        unifiedDiff.setDiffHeader(extractDiffHeader(unifiedDiff.getRawDiff()))
+        parseHeader()
         // TODO: Get the to and from file names from the first line as a last ditch effort
         if (unifiedDiff.getFromFile() == null) {
             unifiedDiff.setFromFile(extractFromFileFromFirstLine())
@@ -126,6 +135,7 @@ class IndividualDiffParser {
         if (unifiedDiff.getToFile() == null) {
             unifiedDiff.setToFile(extractToFileFromFirstLine())
         }
+        // TODO: Doesn't get body for binary patches
         unifiedDiff.setDiffBody(extractDiffBody(unifiedDiff.getRawDiff()))
         return unifiedDiff
     }
@@ -281,6 +291,7 @@ class IndividualDiffParser {
     }
 
     private static String extractDiffBody(String rawDiff) {
+        // XXX: This is incorrect as the diff body won't match this for binary patches.
         String expression = '\n@@'
         Pattern p = Pattern.compile(expression)
         Matcher m = p.matcher(rawDiff)
@@ -293,6 +304,7 @@ class IndividualDiffParser {
     }
 
     private static String extractDiffHeader(String rawDiff) {
+        // XXX: This is incorrect as the diff body won't match this for binary patches.
         String expression = '\n@@'
         Pattern p = Pattern.compile(expression)
         Matcher m = p.matcher(rawDiff)
