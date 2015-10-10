f7a8f20f0f934ce6f7b3d32900b7c2e85acff68d
Scott Giminiani <scottg489@gmail.com>
2015-10-10 19:16:58 -0400

    Implement normal binary strategy.

--- src/main/groovy/udp/strategy/BinaryStrategy.groovy
+++ src/main/groovy/udp/strategy/BinaryStrategy.groovy
@@ -1,10 +1,19 @@
 package udp.strategy
 
+import udp.LineExpression
 import udp.UnifiedDiff
 
 class BinaryStrategy implements LineHandlingStrategy {
     @Override
     void handle(String line, UnifiedDiff unifiedDiff) {
-
+        unifiedDiff.setIsBinary(true)
+        unifiedDiff.setFromFile(extractFromFileFromBinaryLine(line))
+        unifiedDiff.setToFile(extractToFileFromBinaryLine(line))
+    }
+    private static String extractFromFileFromBinaryLine(String binaryFileLine) {
+        return StrategyHelper.extractDataFromHeaderLine(binaryFileLine, LineExpression.BINARY, 2)
+    }
+    private static String extractToFileFromBinaryLine(String binaryFileLine)  {
+        return StrategyHelper.extractDataFromHeaderLine(binaryFileLine, LineExpression.BINARY, 4)
     }
 }
