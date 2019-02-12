decf4dc3877cace3c6f652eca78877c39e3473a2
Scott Giminiani <scottg489@gmail.com>
2019-02-11 21:20:15 -0800

    Remove TODO about diff parser creater using a singleton pattern
    This was fixed during the conversion of the codebase from Groovy to
    Java.

--- src/test/groovy/udp/parse/util/UnifiedDiffParserCreatorTest.groovy
+++ src/test/groovy/udp/parse/util/UnifiedDiffParserCreatorTest.groovy
@@ -3,8 +3,6 @@ package udp.parse.util
 import spock.lang.Specification
 import udp.parse.UnifiedDiffParser
 
-// TODO: Rethink having UnifiedDiffParserCreator being a singleton. Singletons are
-// TODO:    hard to test and may be a bit of an antipattern in general. Investigate.
 class UnifiedDiffParserCreatorTest extends Specification {
     def "A unified diff parser created from a creator should have a node tree"() {
         given:
