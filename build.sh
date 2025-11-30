f3ce41d21d88782eb825c17900b50f0abce6f60f
Scott Giminiani <scottg489@gmail.com>
2025-11-29 21:24:11 -0500

    Add support for building PRs off branches in CI

--- build.sh
+++ build.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
 
-readonly IMAGE_NAME='scottg489/unified-diff-parser-build:latest'
-readonly ID_RSA=$1
-readonly MAVENCENTRAL_USERNAME=$2
-readonly MAVENCENTRAL_PASSWORD=$3
+readonly GIT_BRANCH=${GITHUB_HEAD_REF:-$GITHUB_REF_NAME}
+readonly DOCKER_IMAGE_TAG=$([[ $GIT_BRANCH == "master" ]] && echo -n "latest" || sed 's/[^a-zA-Z0-9]/-/g' <<< "$GIT_BRANCH")
+readonly IMAGE_NAME="scottg489/unified-diff-parser-build:$DOCKER_IMAGE_TAG"
+readonly RUN_TASK=$1
+readonly ID_RSA=$2
+readonly MAVENCENTRAL_USERNAME=$3
+readonly MAVENCENTRAL_PASSWORD=$4
 
 read -r -d '' JSON_BODY <<- EOM
   {
+  "RUN_TASK": "$RUN_TASK",
+  "GIT_BRANCH": "$GIT_BRANCH",
   "ID_RSA": "$ID_RSA",
   "MAVENCENTRAL_USERNAME": "$MAVENCENTRAL_USERNAME",
   "MAVENCENTRAL_PASSWORD": "$MAVENCENTRAL_PASSWORD"
--- infra/build/run.sh
+++ infra/build/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-source /home/build-user/build/build_functions.sh
+source $HOME/build/build_functions.sh
 
 trap cleanup EXIT
 cleanup() {
@@ -16,11 +16,17 @@ set -x
 
 declare -r _PROJECT_NAME='unified-diff-parser'
 declare -r _GIT_REPO='git@github.com:ScottG489/unified-diff-parser.git'
+declare -r _RUN_TASK=$(jq -r .RUN_TASK <<< "$1")
+declare -r _GIT_BRANCH=$(jq -r .GIT_BRANCH <<< "$1")
 
-git clone $_GIT_REPO
+if [ ! -d "$_PROJECT_NAME" ]; then
+  git clone --branch $_GIT_BRANCH $_GIT_REPO
+fi
 cd $_PROJECT_NAME
 
 build_test
+
+[ "$_RUN_TASK" != "deploy" ] && exit 0
 
 set +x
 upload_archives "$1"
